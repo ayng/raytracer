2d1ec2ac972d03339e4321a32af620169390d0ac
Alex Yang <ayyang9@gmail.com>
2016-10-26 18:12:38 -0700

    Adding the forgotten Color class

---
+++ src/Color.cpp
@@ -0,0 +1,52 @@
+#include "Color.h"
+#include <algorithm>
+
+double bound (const double n, const double lower, const double upper) {
+  return std::min(upper, std::max(lower, n));
+}
+
+Color::Color () {
+  r = 0;
+  g = 0;
+  b = 0;
+}
+
+Color::Color (const double _r, const double _g, const double _b) {
+  r = _r;
+  g = _g;
+  b = _b;
+}
+
+Color::Color (const Color& orig) {
+  r = orig.r;
+  g = orig.g;
+  b = orig.b;
+}
+
+Color Color::operator + (const Color& rhs) const {
+  Color res(*this);
+  res.r = bound(rhs.r + res.r, 0.0, 1.0);
+  res.g = bound(rhs.g + res.g, 0.0, 1.0);
+  res.b = bound(rhs.b + res.b, 0.0, 1.0);
+  return res;
+}
+
+Color Color::operator * (const double rhs) const {
+  Color res(*this);
+  res.r = bound(res.r * rhs, 0.0, 1.0);
+  res.g = bound(res.g * rhs, 0.0, 1.0);
+  res.b = bound(res.b * rhs, 0.0, 1.0);
+  return res;
+}
+
+Color Color::operator * (const Color& rhs) const {
+  Color res(*this);
+  res.r = bound(res.r * rhs.r, 0.0, 1.0);
+  res.g = bound(res.g * rhs.g, 0.0, 1.0);
+  res.b = bound(res.b * rhs.b, 0.0, 1.0);
+  return res;
+}
+
+Color operator * (const double lhs, const Color& rhs) {
+  return rhs * lhs;
+}
---
+++ src/Color.h
@@ -0,0 +1,17 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+class Color {
+  public:
+    double r, g, b;
+    Color ();
+    Color (const double _r, const double _g, const double _b);
+    Color (const Color& orig);
+    Color operator + (const Color& rhs) const;
+    Color operator * (const Color& rhs) const;
+    Color operator * (const double rhs) const;
+};
+
+Color operator * (const double lhs, const Color& rhs);
+
+#endif
